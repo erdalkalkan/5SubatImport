8437090493f33e05ecee08c209556fa756506b01
erdalkalkan <83866679+erdalkalkan@users.noreply.github.com>
2024-02-05 17:02:18 +0100

    first commit

---
+++ Core/src/test/java/stepDefinitions/uiStepDef/denemecalass1.java
@@ -0,0 +1,17 @@
+package stepDefinitions.uiStepDef;
+
+import org.junit.Test;
+
+public class denemecalass1 {
+
+
+
+    @Test
+    public void denemeclass(){
+
+        System.out.println("merhaba");
+
+
+
+    }
+}
